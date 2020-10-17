1fa6b068d95571831493b7c895e48dd9c2312647
Jorge Mayorga <jamskar@gmail.com>
2020-10-16 19:35:19 -0500

    Create potencia2elev_i.cpp
    Este desde githubDesktop

---
+++ potencia2elev_i.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    float n,suma=0, potencia=0;
+    do
+    {
+        std::cout<<"ingresa un numero  o numerito> 1 : ";
+        std::cin>>n;    
+
+    } while (n<1);
+    for (int i = 1; i <= n; i++)
+    {
+        potencia = pow(2,i);
+        suma += potencia;
+        std::cout<<"2^"<<i<<"+";
+    }
+    std::cout<<"\n la suma es  "<<suma<<"\n";
+    system("pause");
+    return 0;
+}
